8948fb9f8e282007aef4375a6af51c49bfdeca06
Jan Danisik <jandanisik@gmail.com>
2024-04-11 23:18:21 +0200

    yahoo_finance

---
+++ stocks/data_providers/yahoo_finance.py
@@ -0,0 +1,42 @@
+import yfinance as yf
+import pandas as pd
+
+if __name__ == "__main__":
+
+    # Get the stock ticker
+    ticker = 'AAPL'#input("Enter the stock ticker: ")
+
+    # Get the stock data
+    stock = yf.Ticker(ticker)
+
+    # Get the EPS
+    eps = stock.info['trailingEps']
+    print(f"EPS: {eps}")
+
+    # Get the target price
+    target_price = stock.info['targetMeanPrice']
+    print(f"Target Price: {target_price}")
+
+    # Get the current price
+    current_price = stock.info['currentPrice']
+    print(f"Current Price: {current_price}")
+
+    print(f"Shares: {stock.info['sharesOutstanding']}")
+    print(f"Isin: {stock.isin}")
+
+
+
+    print(f"Strong Sell: {stock.recommendations_summary.strongSell[0]}")
+    print(f"Sell: {stock.recommendations_summary.sell[0]}")
+    print(f"Hold: {stock.recommendations_summary.hold[0]}")
+    print(f"Buy: {stock.recommendations_summary.buy[0]}")
+    print(f"Strong Buy: {stock.recommendations_summary.strongBuy[0]}")
+    print(f"Recommendation: {stock.info['recommendationMean']}")
+    print(f"Industry: {stock.info['industry']}")
+    print(f"Sector: {stock.info['sector']}")
+    print(f"DividendYield: {stock.info['dividendYield']}")
+    print(f"PayoutRatio: {stock.info['payoutRatio']}")
+    #print(f"PayoutRatio: {stock.earnings_forecasts}")
+    print(f"EarningsQuarterlyGrowth: {stock.info['earningsQuarterlyGrowth']}")
+    print(f"EarningsGrowth: {stock.info['earningsGrowth']}")
+
