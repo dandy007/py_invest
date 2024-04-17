99a1e1b0849963ae0851ea652ee8a800ab45d206
Jan Danisik <jandanisik@gmail.com>
2024-04-17 22:56:53 +0200

    types dictionary mapping to tickers

--- stocks/db/__init__.py
+++ stocks/db/__init__.py
@@ -4,4 +4,4 @@ from db.dao_tickers_data import DAO_TickersData
 from db.row_tickers import ROW_Tickers
 from db.row_tickers_data import ROW_TickersData
 from db.db import DB
-from db.constants import TIME_DATA_TYPE_CONST
+from db.constants import TIME_DATA_TYPE_CONST, TICKERS__TYPE_TO_COLUMN__DICT
